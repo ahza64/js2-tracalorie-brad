f905bf52bad2497a0874444aab12359d866a7aad
ahza64 <justinmichaelpettit@gmail.com>
2025-03-20 12:29:02 -0700

    add meals and workouts filter and resets, add set calorie limit

--- js/app.js
+++ js/app.js
@@ -47,6 +47,19 @@ class CalorieTracker {
     }
   }  
 
+  reset() {
+    this._totalCalories = 0
+    this._meals = []
+    this._workouts = []
+    this._render()
+  }
+
+  setLimit(calorieLimit) {
+    this._calorieLimit = calorieLimit
+    this._displayCaloriesLimit()
+    this._render()
+  }
+
   _displayCaloriesTotal() {
     const totalCaloriesEl = document.getElementById('calories-total')
     totalCaloriesEl.innerHTML = this._totalCalories
@@ -170,6 +183,10 @@ class App {
     document.getElementById('workout-form').addEventListener('submit', this._newItem.bind(this, 'workout'))
     document.getElementById('meal-items').addEventListener('click', this._removeItem.bind(this, 'meal'))
     document.getElementById('workout-items').addEventListener('click', this._removeItem.bind(this, 'workout'))
+    document.getElementById('filter-meals').addEventListener('keyup', this._filterItems.bind(this, 'meal'))
+    document.getElementById('filter-workouts').addEventListener('keyup', this._filterItems.bind(this, 'workout'))
+    document.getElementById('reset').addEventListener('click', this._reset.bind(this))
+    document.getElementById('limit-form').addEventListener('submit', this._setLimit.bind(this))
   }
 
   _newItem(type, e) {
@@ -201,6 +218,40 @@ class App {
         e.target.closest('.card').remove()
       }
     }
+  }
+
+  _filterItems(type, e) {
+    const text = e.target.value.toLowerCase()
+    document.querySelectorAll(`#${type}-items .card`).forEach((item) => {
+      const name = item.firstElementChild.firstElementChild.textContent
+      if (name.toLowerCase().indexOf(text) !== -1) {
+        item.style.display = 'block'
+      } else {
+        item.style.display = 'none'
+      }
+    })
+  }
+
+  _reset(e) {
+    this._tracker.reset()
+    document.getElementById('meal-items').innerHTML = ''
+    document.getElementById('workout-items').innerHTML = ''
+    document.getElementById('filter-meals').value = ''
+    document.getElementById('filter-workouts').value = ''
+  }
+
+  _setLimit(e) {
+    e.preventDefault()
+    const limit = document.getElementById('limit')
+    if (limit.value === '') {
+      alert('Please add a calorie limit')
+      return
+    }
+    this._tracker.setLimit(+limit.value)
+    limit.value = ''
+    const modalEl = document.getElementById('limit-modal')
+    const modal = bootstrap.Modal.getInstance(modalEl)
+    modal.hide()
   }
 }
 
